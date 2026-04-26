4ad0468707a885772953ac136a49200311b43f6f
Rhombus_GHIL <rhombusghil@gmail.com>
2026-04-26 19:52:55 +0800

    -
    -

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const express = require('express');
-const request = require('request'); // We need to add this to package.json
 const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// This doesn't process the site, it just provides a clean window
 app.get('/service', (req, res) => {
     const targetUrl = req.query.url;
-    if (!targetUrl) return res.send("No URL provided");
-
-    // This creates a "Tunnel"
-    // Everything the game sends, we pass directly to you
-    req.pipe(request({
-        url: targetUrl,
-        headers: { 'User-Agent': 'Mozilla/5.0' },
-        rejectUnauthorized: false
-    }))
-    .on('error', (err) => res.status(500).send(err.message))
-    .pipe(res); // Send the data to your browser
+    res.send(`
+        <html>
+        <body style="margin:0;">
+            <meta name="referrer" content="no-referrer">
+            <iframe src="${targetUrl}" style="width:100%; height:100vh; border:none;"></iframe>
+        </body>
+        </html>
+    `);
 });
 
 const PORT = process.env.PORT || 10000;
